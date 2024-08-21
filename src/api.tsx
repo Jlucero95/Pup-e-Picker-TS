f566740c3e3a4dab4bcba0463487e78b2fd9b2a0
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2024-08-21 15:56:54 -0500

    Api functions are complete

--- src/api.tsx
+++ src/api.tsx
@@ -1,19 +1,38 @@
+import { Dog } from "./types";
+
 export const baseUrl = "http://localhost:3000";
 
 export const Requests = {
-  // should return a promise with all dogs in the database
-  getAllDogs: () => {},
-  // should create a dog in the database from a partial dog object
-  // and return a promise with the result
-  postDog: () => {},
+	getAllDogs: (): Promise<Dog[]> =>
+		fetch(`${baseUrl}/dogs`).then((dog) => dog.json()),
+	// should create a dog in the database from a partial dog object
+	// and return a promise with the result
+	postDog: async ({ dog }: { dog: Omit<Dog, "id"> }) => {
+		const data = await fetch(`${baseUrl}/dogs`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(dog),
+		});
+		return data.json;
+	},
 
-  // should delete a dog from the database
-  deleteDog: () => {},
+	// should delete a dog from the database
+	deleteDog: ({ dog }: { dog: Dog }) => {
+		return fetch(`${baseUrl}/dogs/${dog.id}`, {
+			method: "DELETE",
+		});
+	},
 
-  updateDog: () => {},
+	updateDog: async ({ dog }: { dog: Dog }) => {
+		const favChange = dog.isFavorite ? false : true;
+		const data = await fetch(`${baseUrl}/dogs/${dog.id}`, {
+			method: "PATCH",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ isFavorite: favChange }),
+		});
+		return data.json;
+	},
 
-  // Just a dummy function for use in the playground
-  dummyFunction: () => {
-    console.log("dummy stuff");
-  },
+	// Just a dummy function for use in the playground
+	dummyFunction: () => {},
 };
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,8 @@
 // Add your own custom types in here
+export type Dog = {
+	id: string;
+	image: string;
+	description: string;
+	isFavorite: boolean;
+	name: string;
+};
